91653caae127043eb2f2d5a86d930765f1ac41ac
Nikolay <hestoperov5@gmail.com>
2022-08-03 15:18:59 +0300

    Внес правки 2-оого усложненного ДЗ

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let num = 262619;
 let arr = ('' + num).split('');
 let arrNew = arr.map(Number);
 let total = 1; 
+
 arrNew.forEach((item, i) => {
     total *= arrNew[i];
 })
+
 console.log('Итог от перемножения цифр = ' + total);
-let total2 = total ** 3;
-console.log('После возведения в 3-юю степень = ' + total2);
-let arrTotal2 = ('' + total2).split('');
-let arrNewTotal = arrTotal2.map(Number);
-let Total3 = arrNewTotal[0] + arrNewTotal[1];
-console.log('Итоговое число:' + Total3);
+let totalSecond = total ** 3;
+
+console.log('После возведения в 3-юю степень = ' + totalSecond);
+console.log('Две первые цифры: ' + String(totalSecond).slice(0,2));
