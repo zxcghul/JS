ad779431055f4474ca27a5d9ccc9931632cf806b
Nikolay <hestoperov5@gmail.com>
2022-08-04 10:30:35 +0300

    Внес правки к 3-ему ДЗ

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
-let title = 'lesson02',
-    screens = 'Простые, Сложные, Интерактивные',
-    screenPrice = 4096,
-    rollback = 20,
-    fullPrice = 8192,
-    adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
-console.log(screens.toLowerCase().split(", "));
-fullPrice = fullPrice * (rollback/100);
-console.log(fullPrice);
-
-title = prompt('Как будет называться ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-
-adaptive = prompt('Нужен ли адаптив на сайте?');
-    if (adaptive == 'Да' || adaptive == 'да') {
-        adaptive = true;
-    }
-    else {
-        adaptive = false;
-    }
-
-service1 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice1 = +prompt('Сколько это будет стоить?');
-service2 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice2 = +prompt('Сколько это будет стоить?');
+const rollback = 10,
+    title = prompt('Как будет называться ваш проект?'),
+    screens = prompt('Какие типы экранов нужно разработать?'),
+    screenPrice = +prompt('Сколько будет стоить данная работа?'),
+    adaptive = !!confirm('Нужен ли адаптив на сайте?'),
+    service1 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice1 = +prompt('Сколько это будет стоить?'),
+    service2 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice2 = +prompt('Сколько это будет стоить?');
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
-servicePercentPrice = Math.ceil((fullPrice*(rollback/100)));
-console.log('Откат посреднику: ' + servicePercentPrice + 'р');
+servicePercentPrice = fullPrice - Math.ceil((fullPrice*(rollback/100)));
+console.log('Итоговая стоимость почсле вычета посреднику: ' + servicePercentPrice + 'р');
 
 switch (true) {
     case fullPrice > 30000:
