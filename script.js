52162d734723d392e3172e813ec2163c66c6e470
Nikolay <hestoperov5@gmail.com>
2022-08-09 13:29:32 +0300

    Начал делать 4-ее дз

---
+++ script.js
@@ -0,0 +1,17 @@
+let allServicePrices, fullPrice;
+    
+    
+
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2
+};
+    allServicePrices = getAllServicePrices();
+
+function getFullPrice() {
+    return screenPrice + allServicePrices
+};
+    fullPrice = getFullPrice();
+
+function getTitle() {
+    
+}
