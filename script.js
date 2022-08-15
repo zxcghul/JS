cab28feed2118a17e10815c47f25ecc2728dda43
Nikola <hestoperov5@gmail.com>
2022-08-15 11:58:54 +0300

    Сделал 6-е усложненное дз

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ function RandomizeGame () {
     alert('Угадай число от 1 до 100');
 
     let number,
-    questAnswer;
+    questAnswer,
+    question,
+    attempts = 2;
 
+
+    function  reboot() {
+        question = confirm('Хотели бы сыграть еще?');
+        if (question === true) {
+            return RandomizeGame ()
+        }
+    }
 
     function isNumber() {
         questAnswer = prompt('Введи своё число:');
-        if (questAnswer === null) {
+        if (questAnswer == null) {
             return alert('Досвидания!');
         }
         if ((isNaN(parseFloat(questAnswer) && isFinite(questAnswer)) || questAnswer == "") == true) {
@@ -22,20 +31,27 @@ function RandomizeGame () {
     }
 
     function isAnswer() {
+        if (attempts == 0) {
+            reboot();
+        }
         if (questAnswer == null) {
             return
         }
         if (questAnswer > number) {
-            alert('Загаданное число меньше!');
+            alert('Загаданное число меньше! \nКол-во попыток = ' + attempts);
+            attempts--;
             isNumber();
             return isAnswer()
         }
         if (questAnswer < number) {
-            alert('Загаданное число больше');
+            alert('Загаданное число больше \nКол-во попыток = ' + attempts);
+            attempts--;
             isNumber();
             return isAnswer()
         }
         alert('Поздравляю, вы угадали!!');
+        reboot();
+        
     }
     
     function getRandom(min, max) {
@@ -46,8 +62,9 @@ function RandomizeGame () {
 
     number = getRandom(1, 100);
     
-    isNumber(questAnswer);
+    isNumber();
     isAnswer();
+    
     
 }
 
